6944fc7690c792c54e61b33254409157235c122e
Brandon Hoang <brandon.hoang@devmounta.in>
2022-07-12 15:40:18 -0500

    Initial commit

---
+++ calc.js
@@ -0,0 +1,37 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?: ", function(input){
+	const tokens = input.split(' ');
+	const num1 = Number(tokens[1]);
+	const num2 = Number(tokens[2]);
+
+
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	   }
+	else if (mathSymbol === "-"){
+		console.log(num1 - num2);
+	}  
+	else if (mathSymbol === "*"){
+		console.log(num1 * num2);
+	}
+	else if (mathSymbol === "/"){
+		console.log(num1 / num2);
+	}
+	else if (mathSymbol === "z"){
+		console.log(Math.sqrt(num1 + num2));
+
+	}
+
+	// This line closes the connection to the command line interface.
+	reader.close();
+
+});
+
